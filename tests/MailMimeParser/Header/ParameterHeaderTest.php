85db878ea14967384eb6426ffc3eafff2a47bf4f
Zaahid Bateson <zbateson@gmail.com>
2015-11-15 20:33:56 -0800

    Rename ValueParametersHeader and refactor
    ParameterHeader extends AbstractHeader.  Added unit tests.

---
+++ tests/MailMimeParser/Header/ParameterHeaderTest.php
@@ -0,0 +1,55 @@
+<?php
+
+use ZBateson\MailMimeParser\Header\Consumer\ConsumerService;
+use ZBateson\MailMimeParser\Header\Part\PartFactory;
+use ZBateson\MailMimeParser\Header\ParameterHeader;
+
+/**
+ * Description of ParametersHeaderTest
+ *
+ * @group Headers
+ * @group ParameterHeader
+ * @author Zaahid Bateson
+ */
+class ParameterHeaderTest extends PHPUnit_Framework_TestCase
+{
+    protected $consumerService;
+    
+    public function setup()
+    {
+        $pf = new PartFactory();
+        $this->consumerService = new ConsumerService($pf);
+    }
+    
+    /*public function testInstance()
+    {
+        $aValid = ['Content-Type', 'CONTENT-Disposition'];
+        $aNot = ['MESSAGE-ID', 'bcc', 'Subject', 'X-Header-Test'];
+        foreach ($aValid as $name) {
+            $header = $this->headerFactory->newInstance($name, 'Test');
+            $this->assertNotNull($header);
+            $this->assertEquals('ZBateson\MailMimeParser\Header\ValueParametersHeader', get_class($header));
+        }
+        foreach ($aNot as $name) {
+            $header = $this->headerFactory->newInstance($name, 'Test');
+            $this->assertNotNull($header);
+            $this->assertNotEquals('ZBateson\MailMimeParser\Header\ValueParametersHeader', get_class($header));
+        }
+    }*/
+    
+    public function testParsingContentType()
+    {
+        $header = new ParameterHeader($this->consumerService, 'Content-Type', 'text/html; CHARSET="utf-8"');
+        $this->assertEquals('text/html', $header->getValue());
+        $this->assertEquals('utf-8', $header->getValueFor('charset'));
+    }
+    
+    public function testParsingMultipleParts()
+    {
+        $header = new ParameterHeader($this->consumerService, 'Content-Type', 'TEXT/html; CHARSET=utf-8; Boundary="blooh";answer-to-everything=42');
+        $this->assertEquals('TEXT/html', $header->getValue());
+        $this->assertEquals('utf-8', $header->getValueFor('charset'));
+        $this->assertEquals('blooh', $header->getValueFor('boundary'));
+        $this->assertEquals('42', $header->getValueFor('answer-to-everything'));
+    }
+}
