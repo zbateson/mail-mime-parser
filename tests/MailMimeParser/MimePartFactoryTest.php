014efa5d208ed77629958374cec810041d31d069
Zaahid Bateson <zbateson@gmail.com>
2016-02-27 14:55:53 -0800

    Add unit test for MimePartFactory

---
+++ tests/MailMimeParser/MimePartFactoryTest.php
@@ -0,0 +1,44 @@
+<?php
+namespace ZBateson\MailMimeParser;
+
+use PHPUnit_Framework_TestCase;
+
+/**
+ * Description of HeaderPartFactoryTest
+ *
+ * @group MimePartFactory
+ * @author Zaahid Bateson
+ */
+class MimePartFactoryTest extends PHPUnit_Framework_TestCase
+{
+    protected $mimePartFactory;
+    
+    protected function setUp()
+    {
+        $headerFactory = $this->getMockBuilder('ZBateson\MailMimeParser\Header\HeaderFactory')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $this->mimePartFactory = new MimePartFactory($headerFactory);
+    }
+    
+    public function testNewMimePart()
+    {
+        $part = $this->mimePartFactory->newMimePart();
+        $this->assertNotNull($part);
+        $this->assertInstanceOf('\ZBateson\MailMimeParser\MimePart', $part);
+    }
+    
+    public function testNewNonMimePart()
+    {
+        $part = $this->mimePartFactory->newNonMimePart();
+        $this->assertNotNull($part);
+        $this->assertInstanceOf('\ZBateson\MailMimeParser\NonMimePart', $part);
+    }
+    
+    public function testNewUUEncodedPart()
+    {
+        $part = $this->mimePartFactory->newUUEncodedPart(066, 'test');
+        $this->assertNotNull($part);
+        $this->assertInstanceOf('\ZBateson\MailMimeParser\UUEncodedPart', $part);
+    }
+}
