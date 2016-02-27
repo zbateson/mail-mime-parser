7f514950e7e4ca8d6882d46a06a089b6fd232dc4
Zaahid Bateson <zbateson@gmail.com>
2016-02-27 15:08:00 -0800

    Add unit tests for UUEncodedPart

---
+++ tests/MailMimeParser/UUEncodedPartTest.php
@@ -0,0 +1,34 @@
+<?php
+namespace ZBateson\MailMimeParser;
+
+use PHPUnit_Framework_TestCase;
+use ZBateson\MailMimeParser\Header\HeaderFactory;
+use ZBateson\MailMimeParser\Header\Consumer\ConsumerService;
+use ZBateson\MailMimeParser\Header\Part\HeaderPartFactory;
+
+/**
+ * Description of UUEncodedPartTest
+ *
+ * @group UUEncodedPart
+ * @author Zaahid Bateson
+ */
+class UUEncodedPartTest extends PHPUnit_Framework_TestCase
+{
+    public function testUUEncodedPartHeadersAndMembers()
+    {
+        $pf = new HeaderPartFactory();
+        $cs = new ConsumerService($pf);
+        $hf = new HeaderFactory($cs, $pf);
+        
+        $part = new UUEncodedPart($hf, 0754, 'test-file.ext');
+        $this->assertNotNull($part);
+        
+        $this->assertEquals('application/octet-stream', $part->getHeaderValue('Content-Type'));
+        $this->assertEquals('test-file.ext', $part->getHeaderParameter('Content-Type', 'name'));
+        $this->assertEquals('x-uuencode', $part->getHeaderValue('Content-Transfer-Encoding'));
+        $this->assertEquals('attachment', $part->getHeaderValue('Content-Disposition'));
+        $this->assertEquals('test-file.ext', $part->getHeaderParameter('Content-Disposition', 'filename'));
+        $this->assertEquals('test-file.ext', $part->getFilename());
+        $this->assertEquals(0754, $part->getUnixFileMode());
+    }
+}
