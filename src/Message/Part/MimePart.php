2c7f48f108a8bc09cdb90d01f6adb330f8870cb2
Zaahid Bateson <zbateson@gmail.com>
2018-05-19 22:12:56 -0500

    Refactor MimePart into traits

--- src/Message/Part/MimePart.php
+++ src/Message/Part/MimePart.php
@@ -8,9 +8,7 @@ namespace ZBateson\MailMimeParser\Message\Part;
 
 use Psr\Http\Message\StreamInterface;
 use ZBateson\MailMimeParser\Header\HeaderFactory;
-use ZBateson\MailMimeParser\Header\ParameterHeader;
 use ZBateson\MailMimeParser\Message\PartFilterFactory;
-use ZBateson\MailMimeParser\Message\PartFilter;
 
 /**
  * Represents a single part of a multi-part mime message.
@@ -25,39 +23,12 @@ use ZBateson\MailMimeParser\Message\PartFilter;
  */
 class MimePart extends MessagePart
 {
-    /**
-     * @var \ZBateson\MailMimeParser\Header\HeaderFactory the HeaderFactory
-     *      object used for created headers
-     */
-    protected $headerFactory;
-    
-    /**
-     * @var \ZBateson\MailMimeParser\Message\PartFilterFactory factory object
-     *      responsible for create PartFilters
-     */
-    protected $partFilterFactory;
-
-    /**
-     * @var \ZBateson\MailMimeParser\Message\Part\MessagePart[] array of child
-     *      parts
-     */
-    protected $children = [];
-    
-    /**
-     * @var string[][] array of headers, with keys set to lower-cased,
-     *      alphanumeric characters of the header's name, and values set to an
-     *      array of 2 elements, the first being the header's original name with
-     *      non-alphanumeric characters and original case, and the second set to
-     *      the header's value.
-     */
-    protected $rawHeaders;
-    
-    /**
-     * @var \ZBateson\MailMimeParser\Header\AbstractHeader[] array of parsed
-     * header objects populated on-demand, the key is set to the header's name
-     * lower-cased, and with non-alphanumeric characters removed.
-     */
-    protected $headers;
+    use MimePartHeaderTrait {
+        MimePartHeaderTrait::__construct as private mimePartHeaderConstructor;
+    }
+    use MimePartChildrenTrait {
+        MimePartChildrenTrait::__construct as private mimePartChildrenConstructor;
+    }
 
     /**
      * Sets up class dependencies.
@@ -78,180 +49,8 @@ class MimePart extends MessagePart
         StreamInterface $contentStream = null
     ) {
         parent::__construct($partStreamFilterManager, $stream, $contentStream);
-
-        $this->headerFactory = $headerFactory;
-        $this->partFilterFactory = $partFilterFactory;
-
-        $pbChildren = $partBuilder->getChildren();
-        if (!empty($pbChildren)) {
-            $this->children = array_map(function ($child) use ($stream) {
-                $childPart = $child->createMessagePart($stream);
-                $childPart->parent = $this;
-                return $childPart;
-            }, $pbChildren);
-        }
-        $this->headers['contenttype'] = $partBuilder->getContentType();
-        $this->rawHeaders = $partBuilder->getRawHeaders();
-    }
-    
-    /**
-     * Returns all parts, including the current object, and all children below
-     * it (including children of children, etc...)
-     * 
-     * @return MessagePart[]
-     */
-    protected function getAllNonFilteredParts()
-    {
-        $parts = [ $this ];
-        foreach ($this->children as $part) {
-            if ($part instanceof MimePart) {
-                $parts = array_merge(
-                    $parts,
-                    $part->getAllNonFilteredParts()
-                );
-            } else {
-                array_push($parts, $part);
-            }
-        }
-        return $parts;
-    }
-
-    /**
-     * Returns the part at the given 0-based index, or null if none is set.
-     * 
-     * Note that the first part returned is the current part itself.  This is
-     * often desirable for queries with a PartFilter, e.g. looking for a
-     * MessagePart with a specific Content-Type that may be satisfied by the
-     * current part.
-     *
-     * @param int $index
-     * @param PartFilter $filter
-     * @return MessagePart
-     */
-    public function getPart($index, PartFilter $filter = null)
-    {
-        $parts = $this->getAllParts($filter);
-        if (!isset($parts[$index])) {
-            return null;
-        }
-        return $parts[$index];
-    }
-
-    /**
-     * Returns the current part, all child parts, and child parts of all
-     * children optionally filtering them with the provided PartFilter.
-     * 
-     * The first part returned is always the current MimePart.  This is often
-     * desirable as it may be a valid MimePart for the provided PartFilter.
-     * 
-     * @param PartFilter $filter an optional filter
-     * @return MessagePart[]
-     */
-    public function getAllParts(PartFilter $filter = null)
-    {
-        $parts = $this->getAllNonFilteredParts();
-        if (!empty($filter)) {
-            return array_values(array_filter(
-                $parts,
-                [ $filter, 'filter' ]
-            ));
-        }
-        return $parts;
-    }
-
-    /**
-     * Returns the total number of parts in this and all children.
-     * 
-     * Note that the current part is considered, so the minimum getPartCount is
-     * 1 without a filter.
-     *
-     * @param PartFilter $filter
-     * @return int
-     */
-    public function getPartCount(PartFilter $filter = null)
-    {
-        return count($this->getAllParts($filter));
-    }
-    
-    /**
-     * Returns the direct child at the given 0-based index, or null if none is
-     * set.
-     *
-     * @param int $index
-     * @param PartFilter $filter
-     * @return MessagePart
-     */
-    public function getChild($index, PartFilter $filter = null)
-    {
-        $parts = $this->getChildParts($filter);
-        if (!isset($parts[$index])) {
-            return null;
-        }
-        return $parts[$index];
-    }
-    
-    /**
-     * Returns all direct child parts.
-     * 
-     * If a PartFilter is provided, the PartFilter is applied before returning.
-     * 
-     * @param PartFilter $filter
-     * @return MessagePart[]
-     */
-    public function getChildParts(PartFilter $filter = null)
-    {
-        if ($filter !== null) {
-            return array_values(array_filter($this->children, [ $filter, 'filter' ]));
-        }
-        return $this->children;
-    }
-    
-    /**
-     * Returns the number of direct children under this part.
-     * 
-     * @param PartFilter $filter
-     * @return int
-     */
-    public function getChildCount(PartFilter $filter = null)
-    {
-        return count($this->getChildParts($filter));
-    }
-
-    /**
-     * Returns the part associated with the passed mime type if it exists.
-     *
-     * @param string $mimeType
-     * @return MessagePart or null
-     */
-    public function getPartByMimeType($mimeType, $index = 0)
-    {
-        $partFilter = $this->partFilterFactory->newFilterFromContentType($mimeType);
-        return $this->getPart($index, $partFilter);
-    }
-    
-    /**
-     * Returns an array of all parts associated with the passed mime type if any
-     * exist or null otherwise.
-     *
-     * @param string $mimeType
-     * @return MessagePart[] or null
-     */
-    public function getAllPartsByMimeType($mimeType)
-    {
-        $partFilter = $this->partFilterFactory->newFilterFromContentType($mimeType);
-        return $this->getAllParts($partFilter);
-    }
-    
-    /**
-     * Returns the number of parts matching the passed $mimeType
-     * 
-     * @param string $mimeType
-     * @return int
-     */
-    public function getCountOfPartsByMimeType($mimeType)
-    {
-        $partFilter = $this->partFilterFactory->newFilterFromContentType($mimeType);
-        return $this->getPartCount($partFilter);
+        $this->mimePartChildrenConstructor($partFilterFactory, $partBuilder, $stream);
+        $this->mimePartHeaderConstructor($headerFactory, $partBuilder);
     }
 
     /**
@@ -367,69 +166,5 @@ class MimePart extends MessagePart
             return $translated[$type];
         }
         return $type;
-    }
-
-    /**
-     * Returns the AbstractHeader object for the header with the given $name
-     *
-     * Note that mime headers aren't case sensitive.
-     *
-     * @param string $name
-     * @return \ZBateson\MailMimeParser\Header\AbstractHeader
-     */
-    public function getHeader($name)
-    {
-        $nameKey = preg_replace('/[^a-z0-9]/', '', strtolower($name));
-        if (isset($this->rawHeaders[$nameKey])) {
-            if (!isset($this->headers[$nameKey])) {
-                $this->headers[$nameKey] = $this->headerFactory->newInstance(
-                    $this->rawHeaders[$nameKey][0],
-                    $this->rawHeaders[$nameKey][1]
-                );
-            }
-            return $this->headers[$nameKey];
-        }
-        return null;
-    }
-
-    /**
-     * Returns the string value for the header with the given $name.
-     *
-     * Note that mime headers aren't case sensitive.
-     *
-     * @param string $name
-     * @param string $defaultValue
-     * @return string
-     */
-    public function getHeaderValue($name, $defaultValue = null)
-    {
-        $header = $this->getHeader($name);
-        if ($header !== null) {
-            return $header->getValue();
-        }
-        return $defaultValue;
-    }
-
-    /**
-     * Returns a parameter of the header $header, given the parameter named
-     * $param.
-     *
-     * Only headers of type
-     * \ZBateson\MailMimeParser\Header\ParameterHeader have parameters.
-     * Content-Type and Content-Disposition are examples of headers with
-     * parameters. "Charset" is a common parameter of Content-Type.
-     *
-     * @param string $header
-     * @param string $param
-     * @param string $defaultValue
-     * @return string
-     */
-    public function getHeaderParameter($header, $param, $defaultValue = null)
-    {
-        $obj = $this->getHeader($header);
-        if ($obj && $obj instanceof ParameterHeader) {
-            return $obj->getValueFor($param, $defaultValue);
-        }
-        return $defaultValue;
     }
 }
---
+++ src/Message/Part/MimePartHeaderTrait.php
@@ -0,0 +1,113 @@
+<?php
+/**
+ * This file is part of the ZBateson\MailMimeParser project.
+ *
+ * @license http://opensource.org/licenses/bsd-license.php BSD
+ */
+namespace ZBateson\MailMimeParser\Message\Part;
+
+use ZBateson\MailMimeParser\Header\HeaderFactory;
+use ZBateson\MailMimeParser\Header\ParameterHeader;
+
+/**
+ * Header related methods attached to a mime part.
+ *
+ * @author Zaahid Bateson
+ */
+trait MimePartHeaderTrait
+{
+    /**
+     * @var \ZBateson\MailMimeParser\Header\HeaderFactory the HeaderFactory
+     *      object used for created headers
+     */
+    protected $headerFactory;
+
+    /**
+     * @var string[][] array of headers, with keys set to lower-cased,
+     *      alphanumeric characters of the header's name, and values set to an
+     *      array of 2 elements, the first being the header's original name with
+     *      non-alphanumeric characters and original case, and the second set to
+     *      the header's value.
+     */
+    protected $rawHeaders;
+
+    /**
+     * @var \ZBateson\MailMimeParser\Header\AbstractHeader[] array of parsed
+     * header objects populated on-demand, the key is set to the header's name
+     * lower-cased, and with non-alphanumeric characters removed.
+     */
+    protected $headers;
+
+    /**
+     * @param \ZBateson\MailMimeParser\Message\Part\HeaderFactory $headerFactory
+     */
+    public function __construct(HeaderFactory $headerFactory, PartBuilder $partBuilder) {
+        $this->headerFactory = $headerFactory;
+        $this->headers['contenttype'] = $partBuilder->getContentType();
+        $this->rawHeaders = $partBuilder->getRawHeaders();
+    }
+
+    /**
+     * Returns the AbstractHeader object for the header with the given $name
+     *
+     * Note that mime headers aren't case sensitive.
+     *
+     * @param string $name
+     * @return \ZBateson\MailMimeParser\Header\AbstractHeader
+     */
+    public function getHeader($name)
+    {
+        $nameKey = preg_replace('/[^a-z0-9]/', '', strtolower($name));
+        if (isset($this->rawHeaders[$nameKey])) {
+            if (!isset($this->headers[$nameKey])) {
+                $this->headers[$nameKey] = $this->headerFactory->newInstance(
+                    $this->rawHeaders[$nameKey][0],
+                    $this->rawHeaders[$nameKey][1]
+                );
+            }
+            return $this->headers[$nameKey];
+        }
+        return null;
+    }
+
+    /**
+     * Returns the string value for the header with the given $name.
+     *
+     * Note that mime headers aren't case sensitive.
+     *
+     * @param string $name
+     * @param string $defaultValue
+     * @return string
+     */
+    public function getHeaderValue($name, $defaultValue = null)
+    {
+        $header = $this->getHeader($name);
+        if ($header !== null) {
+            return $header->getValue();
+        }
+        return $defaultValue;
+    }
+
+    /**
+     * Returns a parameter of the header $header, given the parameter named
+     * $param.
+     *
+     * Only headers of type
+     * \ZBateson\MailMimeParser\Header\ParameterHeader have parameters.
+     * Content-Type and Content-Disposition are examples of headers with
+     * parameters. "Charset" is a common parameter of Content-Type.
+     *
+     * @param string $header
+     * @param string $param
+     * @param string $defaultValue
+     * @return string
+     */
+    public function getHeaderParameter($header, $param, $defaultValue = null)
+    {
+        $obj = $this->getHeader($header);
+        if ($obj && $obj instanceof ParameterHeader) {
+            return $obj->getValueFor($param, $defaultValue);
+        }
+        return $defaultValue;
+    }
+}
