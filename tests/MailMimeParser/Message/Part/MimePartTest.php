cbb55874c71bda57e63e0df6088f89bb05c1410d
Zaahid Bateson <zbateson@gmail.com>
2018-07-24 20:09:11 -0500

    Fix code coverage

--- tests/MailMimeParser/Message/Part/MimePartTest.php
+++ tests/MailMimeParser/Message/Part/MimePartTest.php
@@ -10,8 +10,8 @@ use GuzzleHttp\Psr7;
  * @group MimePart
  * @group MessagePart
  * @covers ZBateson\MailMimeParser\Message\Part\MimePart
- * @covers ZBateson\MailMimeParser\Message\Part\MimePartHeaderTrait
- * @covers ZBateson\MailMimeParser\Message\Part\MimePartChildrenTrait
+ * @covers ZBateson\MailMimeParser\Message\Part\ParentHeaderPart
+ * @covers ZBateson\MailMimeParser\Message\Part\ParentPart
  * @author Zaahid Bateson
  */
 class MimePartTest extends PHPUnit_Framework_TestCase
