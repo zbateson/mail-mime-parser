932e4f65ba3475e7a4049e34da91e5b726b507ad
Michael Voříšek <mvorisek@mvorisek.cz>
2018-09-20 23:51:24 +0200

    Update CharsetConverter.php

--- src/Stream/Helper/CharsetConverter.php
+++ src/Stream/Helper/CharsetConverter.php
@@ -31,6 +31,7 @@ class CharsetConverter
         'CP850' => 'CP850',
         'GB2312' => 'GB2312',
         // aliases
+        'UTF8' => 'UTF-8',
         '646' => 'ASCII',
         'ANSIX341968' => 'ASCII',
         'ANSIX341986' => 'ASCII',
