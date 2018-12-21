43415767e14526104a52ab8aea0ec31f5a0679ca
Zaahid Bateson <zaahid.bateson@ubc.ca>
2018-12-20 16:08:57 -0800

    Fix ignorable space between mime-encoded parts #83

--- tests/MailMimeParser/IntegrationTests/ParserHeadersIntegrationTest.php
+++ tests/MailMimeParser/IntegrationTests/ParserHeadersIntegrationTest.php
@@ -67,5 +67,6 @@ class ParserHeadersIntegrationTest extends PHPUnit_Framework_TestCase
         $this->assertEquals('Andreas Müzner', $message->getHeader('Bcc')->getPersonName());
         
         $this->assertEquals('Технические работы (ERP Галактика и Отчеты ТД)', $message->getHeaderValue('Test'));
+        $this->assertEquals('FAMILY eCarsharing GAUTING STA-CL51E - Buchung geändert Text', $message->getHeaderValue('Subject-X'));
     }
 }
