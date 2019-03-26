63bedd7d71fb3abff2381174a14637c5a4ddaa36
Zaahid Bateson <zaahid.bateson@ubc.ca>
2019-03-26 09:52:07 -0700

    Rename file w/ non-ascii characters, add test #88

---
+++ tests/MailMimeParser/OnlyAsciiFileNamesTest.php
@@ -0,0 +1,32 @@
+<?php
+namespace ZBateson\MailMimeParser;
+
+use PHPUnit\Framework\TestCase;
+use FilesystemIterator;
+use RecursiveDirectoryIterator;
+use RecursiveIteratorIterator;
+
+/**
+ * Some 'unzip' utilities don't like non-ASCII characters appearing in file
+ * names.  Created this test to prevent it from happening as it's happened more
+ * than once.
+ *
+ * @group Base
+ * @author Zaahid Bateson
+ */
+class OnlyAsciiFileNamesTest extends TestCase
+{
+    public function testFileNames()
+    {
+        $dir = new RecursiveDirectoryIterator(dirname(__DIR__), FilesystemIterator::KEY_AS_PATHNAME | FilesystemIterator::SKIP_DOTS);
+        $iter = new RecursiveIteratorIterator($dir);
+        foreach ($iter as $f) {
+            $this->assertTrue(
+                mb_check_encoding($f->getFileName(), 'ASCII'),
+                $f->getFileName() . ' contains non-ascii characters, which may '
+                    . 'cause problems with some \'unzip\' utilities when '
+                    . 'installing via composer'
+            );
+        }
+    }
+}
