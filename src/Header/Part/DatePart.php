0d060efc44718292c480668a37d82b98499e9264
Andrea Ruggiero <pupax@outlook.com>
2019-12-18 14:51:06 +0100

    Handle invalid timezones date

--- src/Header/Part/DatePart.php
+++ src/Header/Part/DatePart.php
@@ -48,6 +48,14 @@ class DatePart extends LiteralPart
                 $date = DateTime::createFromFormat(DateTime::RFC822, $dateToken . 'C');
             }
         }
+        // Missing "+" in timezone definition. eg: Thu, 13 Mar 2014 15:02:47 0000
+        if ($date === false && preg_match('# [0-9]{4}$#', $dateToken)) {
+            $fixedDate = preg_replace('# ([0-9]{4})$#', ' +$1', $dateToken);
+            $date = DateTime::createFromFormat(DateTime::RFC2822, $fixedDate);
+            if ($date === false) {
+                $date = DateTime::createFromFormat(DateTime::RFC822, $fixedDate);
+            }
+        }
         if ($date !== false) {
             if ($date->format('Y') < 50) {
                 $date->setDate(intval($date->format('Y')) + 2000, $date->format('m'), $date->format('d'));
