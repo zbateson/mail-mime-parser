d819761d4ee12942b4428a89b96b57184a812fab
Thomas Landauer <thomas@landauer.at>
2020-08-05 00:37:45 +0200

    Create Header.php
    First step of https://github.com/zbateson/mail-mime-parser/issues/121

---
+++ src/Header/Header.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * This file is part of the ZBateson\MailMimeParser project.
+ *
+ * @license http://opensource.org/licenses/bsd-license.php BSD
+ */
+namespace ZBateson\MailMimeParser\Header;
+
+abstract class Header
+{
+    const FROM = 'from';
+}
