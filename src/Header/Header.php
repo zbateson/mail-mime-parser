73004c0ef019691726cc412376d54fdcb12c797a
Thomas Landauer <thomas@landauer.at>
2020-10-07 20:26:30 +0200

    Update Header.php

--- src/Header/Header.php
+++ src/Header/Header.php
@@ -8,18 +8,33 @@ namespace ZBateson\MailMimeParser\Header;
 
 abstract class Header
 {
-    const FROM = 'from';
-    const TO = 'to';
-    const SUBJECT = 'subject';
-    const MESSAGE_ID = 'message-id';
-    const CONTENT_TYPE = 'content-type';
-    const CC = 'cc';
-    const BCC = 'bcc';
-    const DATE = 'date';
-    const SENDER = 'sender';
-    const REPLY_TO = 'reply-to';
-    const IN_REPLY_TO = 'in-reply-to';
-    const REFERENCES = 'references';
-    const AUTO_SUBMITTED = 'auto-submitted'; // https://tools.ietf.org/html/rfc3834#section-5
-    const MIME_VERSION = 'mime-version'; // https://tools.ietf.org/html/rfc2045#section-4
+    // Headers according to the table at https://tools.ietf.org/html/rfc5322#section-3.6
+    const RETURN_PATH = 'Return-Path';
+    const RECEIVED = 'Received';
+    const RESENT_DATE = 'Resent-Date';
+    const RESENT_FROM = 'Resent-From';
+    const RESENT_SENDER = 'Resent-Sender';
+    const RESENT_TO = 'Resent-To';
+    const RESENT_CC = 'Resent-Cc';
+    const RESENT_BCC = 'Resent-Bcc';
+    const RESENT_MSD_ID = 'Resent-Message-ID';
+    const RESENT_MESSAGE_ID = self::RESENT_MSD_ID;
+    const ORIG_DATE = 'Date'; // orig-date
+    const DATE = self::ORIG_DATE;
+    const FROM = 'From';
+    const SENDER = 'Sender';
+    const REPLY_TO = 'Reply-To'; 
+    const TO = 'To';
+    const CC = 'Cc';
+    const BCC = 'Bcc';
+    const MESSAGE_ID = 'Message-ID';
+    const IN_REPLY_TO = 'In-Reply-To';
+    const REFERENCES = 'References'; 
+    const SUBJECT = 'Subject';
+    const COMMENTS = 'Comments';
+    const KEYWORDS = 'Keywords';
+    
+    const MIME_VERSION = 'MIME-Version'; // https://tools.ietf.org/html/rfc2045#section-4
+    const CONTENT_TYPE = 'Content-Type'; // https://tools.ietf.org/html/rfc2045#section-5
+    const AUTO_SUBMITTED = 'Auto-Submitted'; // https://tools.ietf.org/html/rfc3834#section-5
 }
