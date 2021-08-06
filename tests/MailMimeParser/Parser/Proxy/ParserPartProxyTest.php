edad5f71bc74db4d6f96a2c40a04ef07fddb27aa
Zaahid Bateson <zbateson@users.noreply.github.com>
2021-08-06 16:31:44 -0700

    Add tests

--- tests/MailMimeParser/Parser/Proxy/ParserPartProxyTest.php
+++ tests/MailMimeParser/Parser/Proxy/ParserPartProxyTest.php
@@ -2,7 +2,6 @@
 namespace ZBateson\MailMimeParser\Parser\Proxy;
 
 use LegacyPHPUnit\TestCase;
-use GuzzleHttp\Psr7\Utils;
 
 /**
  * ParserPartProxyTest
@@ -16,19 +15,28 @@ class ParserPartProxyTest extends TestCase
 {
     private $partBuilder;
     private $childParser;
+    private $parentParser;
     private $parent;
 
     protected function legacySetUp()
     {
-        $this->partBuilder = $this->getMockBuilder('ZBateson\MailMimeParser\Parser\PartBuilder')
-            ->disableOriginalConstructor()
-            ->getMock();
+        $pbm = $this->getMockBuilder('ZBateson\MailMimeParser\Parser\PartBuilder')
+            ->disableOriginalConstructor();
+        $this->partBuilder = $pbm->getMock();
         $this->childParser = $this->getMockBuilder('ZBateson\MailMimeParser\Parser\IParser')
             ->disableOriginalConstructor()
             ->getMockForAbstractClass();
-        $this->parent = $this->getMockBuilder('ZBateson\MailMimeParser\Parser\Proxy\ParserPartProxy')
+        $this->parentParser = $this->getMockBuilder('ZBateson\MailMimeParser\Parser\IParser')
             ->disableOriginalConstructor()
-            ->getMock();
+            ->getMockForAbstractClass();
+        
+        $this->parent = $this->getMockBuilder('ZBateson\MailMimeParser\Parser\Proxy\ParserMimePartProxy')
+            ->setConstructorArgs([
+                $this->getMockBuilder('ZBateson\MailMimeParser\Message\PartHeaderContainer')->disableOriginalConstructor()->getMock(),
+                $pbm->getMock(),
+                $this->parentParser,
+                null
+            ])->getMock();
     }
 
     public function testSetGetPart()
@@ -41,5 +49,60 @@ class ParserPartProxyTest extends TestCase
         $this->assertSame($part, $instance->getPart());
     }
 
-    
+    public function testParseContent()
+    {
+        $instance = new ParserPartProxy($this->partBuilder, $this->childParser);
+        $this->partBuilder
+            ->expects($this->any())
+            ->method('isContentParsed')
+            ->willReturnOnConsecutiveCalls(false, true);
+        $this->childParser
+            ->expects($this->once())
+            ->method('parseContent')
+            ->with($instance);
+
+        $instance->parseContent();
+        $instance->parseContent();
+    }
+
+    public function testParseContentWithParentParser()
+    {
+        $instance = new ParserPartProxy($this->partBuilder, $this->childParser, $this->parent);
+        $this->partBuilder
+            ->expects($this->any())
+            ->method('isContentParsed')
+            ->willReturnOnConsecutiveCalls(false, true);
+
+        $this->childParser
+            ->expects($this->never())
+            ->method('parseContent');
+        $this->parentParser
+            ->expects($this->once())
+            ->method('parseContent')
+            ->with($instance);
+
+        $instance->parseContent();
+        $instance->parseContent();
+    }
+
+    public function testParseAllParsesContent()
+    {
+        $instance = new ParserPartProxy($this->partBuilder, $this->childParser);
+        $this->partBuilder
+            ->expects($this->any())
+            ->method('isContentParsed')
+            ->willReturnOnConsecutiveCalls(false, true);
+
+        $this->childParser
+            ->expects($this->once())
+            ->method('parseContent');
+        $instance->parseAll();
+        $instance->parseAll();
+    }
+
+    public function testGetPartBuilder()
+    {
+        $instance = new ParserPartProxy($this->partBuilder, $this->childParser);
+        $this->assertSame($this->partBuilder, $instance->getPartBuilder());
+    }
 }
