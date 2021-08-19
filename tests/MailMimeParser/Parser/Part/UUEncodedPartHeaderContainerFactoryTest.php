b4f764206d0289c6dc3297ff16e6f446ce6c6387
Zaahid Bateson <zbateson@users.noreply.github.com>
2021-08-18 22:31:01 -0700

    Add tests

---
+++ tests/MailMimeParser/Parser/Part/UUEncodedPartHeaderContainerFactoryTest.php
@@ -0,0 +1,37 @@
+<?php
+namespace ZBateson\MailMimeParser\Parser\Part;
+
+use LegacyPHPUnit\TestCase;
+
+/**
+ * UUEncodedPartHeaderContainerFactoryTest
+ *
+ * @group UUEncodedPartHeaderContainerFactory
+ * @group Parser
+ * @covers ZBateson\MailMimeParser\Parser\Part\UUEncodedPartHeaderContainerFactory
+ * @author Zaahid Bateson
+ */
+class UUEncodedPartHeaderContainerFactoryTest extends TestCase
+{
+    private $instance;
+
+    protected function legacySetUp()
+    {
+        $hf = $this->getMockBuilder('ZBateson\MailMimeParser\Header\HeaderFactory')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $this->instance = new UUEncodedPartHeaderContainerFactory($hf);
+    }
+
+    public function testNewInstance()
+    {
+        $ob = $this->instance->newInstance(0777, 'test0r');
+        $this->assertInstanceOf(
+            '\ZBateson\MailMimeParser\Parser\Part\UUEncodedPartHeaderContainer',
+            $ob
+        );
+        // make sure params are passed
+        $this->assertSame(0777, $ob->getUnixFileMode());
+        $this->assertSame('test0r', $ob->getFilename());
+    }
+}
---
+++ tests/MailMimeParser/Parser/Part/UUEncodedPartHeaderContainerTest.php
@@ -0,0 +1,37 @@
+<?php
+namespace ZBateson\MailMimeParser\Parser\Part;
+
+use LegacyPHPUnit\TestCase;
+
+/**
+ * UUEncodedPartHeaderContainerTest
+ *
+ * @group UUEncodedPartHeaderContainer
+ * @group Parser
+ * @covers ZBateson\MailMimeParser\Parser\Part\UUEncodedPartHeaderContainer
+ * @author Zaahid Bateson
+ */
+class UUEncodedPartHeaderContainerTest extends TestCase
+{
+    private $instance;
+
+    protected function legacySetUp()
+    {
+        $hf = $this->getMockBuilder('ZBateson\MailMimeParser\Header\HeaderFactory')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $this->instance = new UUEncodedPartHeaderContainer($hf);
+    }
+
+    public function testGetSetUnixFileMode()
+    {
+        $this->instance->setUnixFileMode(0777);
+        $this->assertSame(0777, $this->instance->getUnixFileMode());
+    }
+
+    public function testGetSetFilename()
+    {
+        $this->instance->setFilename('test0r');
+        $this->assertSame('test0r', $this->instance->getFilename());
+    }
+}
