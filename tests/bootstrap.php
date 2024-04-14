1a5922284ab118fdb04dbfe60b3bd60f9478487b
Zaahid Bateson <zbateson@users.noreply.github.com>
2024-04-14 16:28:33 -0700

    Setup test logger

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,3 +1,4 @@
 <?php
 
 $loader = require_once \dirname(__DIR__) . '/vendor/autoload.php';
+require_once __DIR__ . '/setup-test-logger.php';
---
+++ tests/setup-test-logger.php
@@ -0,0 +1,26 @@
+<?php
+
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+use Monolog\Handler\TestHandler;
+use Monolog\Processor\PsrLogMessageProcessor;
+
+function mmpGetAndClearTestHandler() : TestHandler {
+    static $handler = null;
+    if ($handler === null) {
+        $handler = new TestHandler('debug');
+    }
+    $handler->clear();
+    return $handler;
+}
+
+function mmpGetTestLogger() : Logger {
+    static $logger = null;
+    if ($logger === null) {
+        $logger = new Logger('testing');
+        $logger->pushProcessor(new PsrLogMessageProcessor(removeUsedContextFields: true));
+        $logger->pushHandler(new StreamHandler(__DIR__ . '/' . TEST_LOG_FILE, 'debug'));
+        $logger->pushHandler(mmpGetAndClearTestHandler());
+    }
+    return $logger;
+}
